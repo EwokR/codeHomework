c43854fcce66a91c4178ea69f61f34d6f122ecce
Anton <imewokrus@gmail.com>
2022-09-15 18:52:44 +0300

    Fifth commit

--- src/main/java/com/example/codehomework/scripts.sql
+++ src/main/java/com/example/codehomework/scripts.sql
@@ -1,9 +1,9 @@
 SELECT *FROM student;
-SELECT name, age, id FROM student
+SELECT  *FROM student
 WHERE age between 10 and 20;
 SELECT name FROM student;
-SELECT name, age, id FROM student
+SELECT *FROM student
 WHERE name like ('%s%');
-SELECT name, age, id FROM student where age < 16;
-SELECT name, age, id FROM student
+SELECT *FROM student where age < 16;
+SELECT *FROM student
 ORDER BY age;
