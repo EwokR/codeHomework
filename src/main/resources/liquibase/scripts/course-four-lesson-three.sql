590def501cec4cb96642a928c24d4ddc0ecbbd82
Anton <imewokrus@gmail.com>
2022-10-25 13:54:17 +0300

    Thirteenth commit

---
+++ src/main/resources/liquibase/scripts/course-four-lesson-three.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+
+-- changeset AKorneev: 1
+
+CREATE INDEX student_name_index on student (name);
+
+CREATE INDEX faculty_name_index on faculty (name);
+
+CREATE INDEX faculty_colour_index on faculty (colour);
